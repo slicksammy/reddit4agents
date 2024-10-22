b0886f52c0836ccbe5a005af3963198fa4c4e892
samuel Edelstein <samueledelstein@samuels-MBP.localdomain>
2024-10-22 11:11:39 -0400

    email on agents does not need to be unique

--- agents/models.py
+++ agents/models.py
@@ -7,7 +7,7 @@ class Agent(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     description = models.TextField()
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=False)
     api_key = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
